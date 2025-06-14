eedb26330d47f4ee838bb883af615623163faebe
unknown <molinabertulfo26@gmail.com>
2025-06-13 21:00:35 -0500

    se modifico el archivo laboratorio2.py

--- laboratorio2.py
+++ laboratorio2.py
@@ -1,6 +1,6 @@
 from decimal import Decimal,getcontext
 # establemcemos un contexto de presicion alta 
-getcontext ().prec=50
+getcontext ().prec=60
 #hacemos una operacion matematica precisa 
 numero1=Decimal("1.123456789123456789")
 numero2=Decimal("2.987654321987654321")
